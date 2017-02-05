7a99bcd542b1e040cc5e29b90b14eb156e2903eb
Stephen Zhu <songzhezhu@email.arizona.edu>
2017-02-05 16:26:48 -0700

    Added a 2nd view that use JTextField, a JButton, and a JTextArea

---
+++ src/view/TextFieldView.java
@@ -0,0 +1,199 @@
+package view;
+
+/*
+ * Name: Songzhe Zhu
+ * Date: Feb 5 2017
+ * Purpose: This is the second view of the TicTacToe game which is
+ * 			a text field version.
+ */
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import controller.OurObserver;
+import model.ComputerPlayer;
+import model.TicTacToeGame;
+
+@SuppressWarnings("serial")
+public class TextFieldView extends JPanel implements OurObserver {
+	private TicTacToeGame Game;
+	private JButton GameButton = new JButton("Make the move");
+	private JTextField Row = new JTextField("");
+	private JTextField Col = new JTextField("");
+	private JLabel row = new JLabel("row");
+	private JLabel col = new JLabel("column");
+	private JTextArea GridArea = new JTextArea(3, 3);
+	private ComputerPlayer AI;
+	private char[][] Grid = null;
+	private String tmp = "";
+	private int height = 0;
+	private int width = 0;
+
+	public TextFieldView(TicTacToeGame TicTacToeGame, int wide, int high) {
+		this.Game = TicTacToeGame;
+		this.width = wide;
+		this.height = high;
+		this.AI = Game.getComputerPlayer();
+		initializeButtonPanel();
+	}
+
+	private void initializeButtonPanel() {
+		// TODO Auto-generated method stub
+		// Control has two labels, text for row and column, and a button
+		JPanel Control = new JPanel();
+		// Field has two lables and text for row and column
+		JPanel Field = new JPanel();
+		int size = Game.size();
+
+		this.setLayout(null);
+		Field.setLayout(new GridLayout(size, size, 5, 5));
+		Field.setLocation(10, 5);
+		Field.setSize(500, 100);
+		Field.setBackground(Color.BLUE);
+		Field.add(Row);
+		Field.add(row);
+		Field.add(Col);
+		Field.add(col);
+
+		Control.setSize(width - 30, height - 300);
+		Control.setLocation(10, 5);
+		Control.setBackground(Color.BLUE);
+
+		ButtonListener aListener = new ButtonListener();
+		GameButton.setSize(120, 30);
+		GameButton.setLocation(110, 20);
+		GameButton.setBackground(Color.CYAN);
+		GameButton.setFont(new Font("Arial", Font.BOLD, 18));
+		GameButton.addActionListener(aListener);
+
+		Control.add(Field);
+		Control.add(GameButton);
+		this.add(Control);
+
+		Grid = new char[size][size];
+		for (int i = 0; i < size; i++) {
+			tmp += " ";
+			for (int j = 0; j < size; j++) {
+				Grid[i][j] = '_';
+				tmp += Grid[i][j] + " ";
+			}
+			tmp += "\n";
+		}
+
+		GridArea.setLayout(new FlowLayout());
+		GridArea.setText(tmp);
+		GridArea.setSize(width - 120, height - 120);
+		GridArea.setLocation(40, 70);
+		GridArea.setFont(new Font("Courier", Font.BOLD, 36));
+		this.setBackground(Color.CYAN);
+		this.add(GridArea);
+	}
+
+	//Action when player click the button
+	private class ButtonListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			String tmprow = Row.getText();
+			String tmpcol = Col.getText();
+			int tmpRow = 0;
+			int tmpCol = 0;
+			
+			tmpRow = Integer.parseInt(tmprow);
+			tmpCol = Integer.parseInt(tmpcol);
+			
+			if(tmpRow >= 0 && tmpRow < 3 && tmpCol >= 0 && tmpCol < 3 && Grid[tmpRow][tmpCol] == '_'){
+				Game.choose(tmpRow, tmpCol);
+				if(Game.tied()){
+					GameButton.setText("Tied");
+					GameButton.setEnabled(false);
+					updateField();
+				}
+				else if(Game.didWin('X')){
+					GameButton.setText("X Wins");
+					GameButton.setEnabled(false);
+					updateField();
+				}
+				else{
+					Point next = AI.desiredMove(Game);
+					Game.choose(next.x, next.y);
+					if(Game.didWin('O')){
+						GameButton.setText("O Wins");
+						updateField();
+					}
+				}
+			}
+			else{
+				JOptionPane.showMessageDialog(null, "Selection not available");
+			}
+		}
+	}
+	
+	// This method is called by OurObservable's notifyObservers()
+	public void update() {
+		if (Game.maxMovesRemaining() == Game.size() * Game.size())
+			resetField(true);
+
+		if (!Game.stillRunning())
+			resetField(false);
+		else {
+			updateField();
+			GameButton.setText("Make the move");
+		}
+	}
+
+	//update Field after one move
+	private void updateField() {
+		// TODO Auto-generated method stub
+		Grid = Game.getTicTacToeBoard();
+		int size = Game.size();
+		tmp = "";
+		for(int i = 0; i < size; i++){
+			tmp += " ";
+			for(int j = 0; j < size; j++){
+				tmp += Grid[i][j] + " ";
+			}
+			tmp += "\n";
+		}
+		GridArea.setText(tmp);
+	}
+
+	//reset Field when a new game start
+	private void resetField(boolean bool) {
+		// TODO Auto-generated method stub
+		if(bool){
+			int size = Game.size();
+			Grid = new char[size][size];
+			tmp = "";
+			for(int i = 0; i < size; i++){
+				tmp += " ";
+				for(int j = 0; j < size; j++){
+					tmp += Grid[i][j] + " ";
+				}
+				tmp += "\n";
+			}
+			GridArea.setText(tmp);	
+		}
+		else{
+			if(Game.tied())
+				GameButton.setText("Tied");
+			else if(Game.didWin('X'))
+				GameButton.setText("X Wins");
+			else
+				GameButton.setText("O Wins");
+		}
+		GameButton.setEnabled(bool);
+	}
+}
